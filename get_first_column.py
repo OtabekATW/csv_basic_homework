491d0854109b5f281dd63bcbd1554ea348ae8484
OtabekATW <otabektursunov468@gmail.com>
2023-12-15 15:49:45 +0500

    get_first_column.py

--- get_first_column.py
+++ get_first_column.py
@@ -1,3 +1,4 @@
+import csv
 def get_first_column(data):
     """
     Get the first column from a CSV file.
@@ -6,6 +7,14 @@ def get_first_column(data):
     Return:
         list: First column.
     """
-    return
+    f = open('data.csv')
+    data = csv.reader(f)
+
+    first_column = []
+    for i in data:
+        temp = (', '.join(i)).split(',')
+        first_column.append(temp[0])
+    
+    return first_column
     
 # Read the csv file
